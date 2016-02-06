a8326a305d563265015e7abbf457e8b168012808
Ubuntu <ubuntu@ip-172-31-0-213.us-west-2.compute.internal>
2016-02-06 19:44:52 +0000

    Updated Kafka streaming dns

--- streaming/run_streaming.sh
+++ streaming/run_streaming.sh
@@ -1,1 +1,1 @@
-spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.5.1 --master spark://ip-172-31-0-200:7077 smw_stream.py "ec2-52-89-146-18.us-west-2.compute.amazonaws.com:9092"
+spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.5.1 --master spark://ip-172-31-0-213:7077 smw_stream.py "ec2-54-148-53-88.us-west-2.compute.amazonaws.com:9092"
